a0965ec235ab26418ed0d87e55c59211d8bfdc67
Mike Rousos <mikerou@microsoft.com>
2022-12-03 16:23:50 -0500

    Add day 2 solutions

---
+++ 2022/2/Part1.cs
@@ -0,0 +1,28 @@
+﻿public static class Part1
+{
+    public async static Task ExecuteAsync()
+    {
+        var score = 0;
+        foreach (var line in await File.ReadAllLinesAsync("input.txt"))
+        {
+            score += line switch
+            {
+                "A X" => 1 + 3,
+                "A Y" => 2 + 6,
+                "A Z" => 3 + 0,
+
+                "B X" => 1 + 0,
+                "B Y" => 2 + 3,
+                "B Z" => 3 + 6,
+
+                "C X" => 1 + 6,
+                "C Y" => 2 + 0,
+                "C Z" => 3 + 3,
+
+                _ => 0
+            };
+        }
+
+        Console.WriteLine(score);
+    }
+}
---
+++ 2022/2/Part2.cs
@@ -0,0 +1,28 @@
+﻿public static class Part2
+{
+    public async static Task ExecuteAsync()
+    {
+        var score = 0;
+        foreach (var line in await File.ReadAllLinesAsync("input.txt"))
+        {
+            score += line switch
+            {
+                "A X" => 3 + 0,
+                "A Y" => 1 + 3,
+                "A Z" => 2 + 6,
+
+                "B X" => 1 + 0,
+                "B Y" => 2 + 3,
+                "B Z" => 3 + 6,
+
+                "C X" => 2 + 0,
+                "C Y" => 3 + 3,
+                "C Z" => 1 + 6,
+
+                _ => 0
+            };
+        }
+
+        Console.WriteLine(score);
+    }
+}
