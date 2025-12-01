e67a86175c0a185dc7248e27eab641a9fa07e882
Mike Rousos <mike@mjrousos.com>
2025-12-01 10:43:02 -0500

    Add 2025 day 1

---
+++ 2025/cs/day1/Program.cs
@@ -0,0 +1,62 @@
+﻿const int DialSpace = 100;
+
+var input = await File.ReadAllLinesAsync("input.txt");
+
+var position = 50;
+Console.WriteLine($"Initial Position: {position}");
+
+var zeroCount = 0;
+var passZeroCount = 0;
+foreach (var line in input)
+{
+    (position, var crossZero) = Adjust(position, line);
+    
+    passZeroCount += crossZero;
+
+    if (position == 0)
+    {
+        zeroCount++;
+    }
+}
+
+Console.WriteLine($"Zero count: {zeroCount}");
+Console.WriteLine($"Pass zero count: {passZeroCount + zeroCount}");
+
+static (int, int) Adjust(int pos, string command)
+{
+    var left = command[0] switch
+    {
+        'L' => true,
+        'R' => false,
+        _ => throw new InvalidOperationException("Invalid command")
+    };
+
+    if (left && pos == 0)
+    {
+        pos = 100;
+    }
+
+    if (!int.TryParse(command[1..], out var increment))
+    {
+        throw new InvalidOperationException("Invalid increment");
+    }
+
+    var newPosition = left ? pos - increment : pos + increment;
+
+    var crossZero = 0;
+    while (newPosition < 0)
+    {
+        crossZero++;
+        newPosition += DialSpace;
+    }
+
+    // > rather than >= to avoid counting landing on zero as crossing zero
+    while (newPosition > DialSpace)
+    {
+        crossZero++;
+        newPosition -= DialSpace;
+    }
+
+    // % is required despite the while loops to handle cases where newPosition == 100
+    return (newPosition % DialSpace, crossZero);
+}
